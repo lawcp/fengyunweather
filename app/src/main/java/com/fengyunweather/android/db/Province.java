5089dc64e03a2fc76e043a4b5104b557d16f3889
xu <502775543@qq.com>
2017-03-07 21:28:16 +0800

    修改本地不能建表的bug

--- app/src/main/java/com/fengyunweather/android/db/Province.java
+++ app/src/main/java/com/fengyunweather/android/db/Province.java
@@ -9,7 +9,7 @@ import org.litepal.crud.DataSupport;
 public class Province extends DataSupport {
     private int id;
     private String provinceName;
-
+    private int provinceCode;
     public int getId() {
         return id;
     }
@@ -34,6 +34,6 @@ public class Province extends DataSupport {
         this.provinceCode = provinceCode;
     }
 
-    private int provinceCode;
+
 
 }
